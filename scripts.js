5513d0ea777476cc0579bf914386df18ded8fdcf
bdrhnkr <102805585+bdrhnkr@users.noreply.github.com>
2022-04-20 15:45:51 +0300

    Add files via upload
    'blog' page coded. Couldn't made responsible yet.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,23 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:3,
-            nav:true,
-            loop:false
-        }
-    }
-})
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+		margin:10,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:1,
+				nav:true
+			},
+			600:{
+				items:3,
+				nav:false
+			},
+			1000:{
+				items:3,
+				nav:true,
+				loop:false
+			}
+		}
+	})
+
 });
